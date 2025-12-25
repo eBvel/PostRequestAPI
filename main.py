cc28f94800ddcd5ea69bfae4e8c4f7f47eb379b6
eBvel <belyakov041098@gmail.com>
2025-12-25 15:45:04 +0700

    Made changes to the main method.

--- main.py
+++ main.py
@@ -1,76 +1,36 @@
-import requests
+from data import GoogleMapConfig
+from file_manager import FileManager
+from service import GoogleMapService
+from tests import TestGoogleMapAPI
 
 
-class TestGoogleMapAPI:
+def main():
+    print("\nSTART TESTING\n")
 
-    base_url = "https://rahulshettyacademy.com"
-    key_param = "?key=qaclick123"
+    file_path = "list_of_place_id.txt"
+    api_service = GoogleMapService(GoogleMapConfig())
+    api_test = TestGoogleMapAPI(api_service)
 
-    def _compare_status_code(self, result_code, expected_code):
-        try:
-            print(f"Response status-code: {result_code}")
-            assert result_code == expected_code
-            print("PASSED: Status is correct!")
-        except AssertionError:
-            print("FAILED: Status is incorrect!")
+    """
+    Выполняем POST-тест на запись локации 5 раз. Записываем place_id локации 
+    в переменную file_data. Значенеие переменной записываем в файл 
+    list_of_place_id.
+    """
+    file_data = ""
+    for i in range(5):
+        file_data += api_test.test_create_location(200) + "\n"
+    FileManager.write(file_path, file_data)
 
-    def test_create_new_location(self, body_json):
-        post_url = self.base_url + "/maps/api/place/add/json" + self.key_param
-        print(f"POST URL: {post_url}")
+    """
+    Считываем значение список place_id из файла list_of_place_id.
+    Выполняем GET-тест для поиска локаций по идентификатору(place_id).
+    """
+    list_of_place_id = FileManager.read_line_by_line(file_path)
+    for place_id in list_of_place_id:
+        api_test.test_find_location(place_id, 200)
 
-        response = requests.post(post_url, json=body_json)
-        print(f"RESPONSE:\n{response.json()}")
-
-        self._compare_status_code(response.status_code, 200)
-
-        status = response.json().get("status")
-        print(f"Response Status: {status}")
-        assert status == "OK", "FAILED: Status is incorrect!"
-        print("PASSED: Status is correct!")
-
-        return response.json().get("place_id")
-
-    def test_find_location_by_place_id(self, place_id):
-        get_url = (self.base_url + "/maps/api/place/get/json"
-                   + self.key_param + f"&place_id={place_id}")
-        print(f"GET URL: {get_url}\nPlace id: {place_id}")
-
-        response = requests.get(get_url)
-        print(f"RESPONSE:\n{response.json()}")
-
-        self._compare_status_code(response.status_code, 200)
-        print("LOCATION FOUND")
+    print("\nTEST PASSED")
 
 
-def read_lines(file_path):
-    with open(file_path, 'r', encoding="UTF-8") as file:
-        for line in file:
-            yield line
-
-file_name = "list_of_place_id.txt"
-request_body = {
-    "location": {
-        "lat": -38.383494,
-        "lng": 33.427362
-    },
-    "accuracy": 50,
-    "name": "Frontline house",
-    "phone_number": "(+91) 983 893 3937",
-    "address": "29, side layout, cohen 09",
-    "types": ["shoe park","shop"],
-    "website": "http://google.com",
-    "language": "French-IN"
-}
-test_map = TestGoogleMapAPI()
-
-result = ""
-for i in range(5):
-    result += f"{test_map.test_create_new_location(request_body)}\n"
-
-with open(file_name, 'w', encoding="UTF-8") as file:
-    file.write(f"{result}")
-
-for id in read_lines(file_name):
-    test_map.test_find_location_by_place_id(id.replace("\n", ""))
-
-print("\nTEST PASSED")
+if __name__ == "__main__":
+    main()
