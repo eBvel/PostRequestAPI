e2e16f8a342bda62bfe6c4f936ccc0ad9718289b
eBvel <belyakov041098@gmail.com>
2025-12-29 15:30:49 +0700

    Separated tests and main logic.

--- main.py
+++ main.py
@@ -5,29 +5,26 @@ from tests import TestGoogleMapAPI
 
 
 def main():
-    print("\nSTART TESTING\n")
+    print("\nSTART TESTING")
 
     file_path = "list_of_place_id.txt"
     api_service = GoogleMapService(GoogleMapConfig())
     api_test = TestGoogleMapAPI(api_service)
 
-    """
-    Выполняем POST-тест на запись локации 5 раз. Записываем place_id локации 
-    в переменную file_data. Значенеие переменной записываем в файл 
-    list_of_place_id.
-    """
-    file_data = ""
-    for i in range(5):
-        file_data += api_test.test_create_location(200) + "\n"
-    FileManager.write(file_path, file_data)
+    # Проверяем создание (POST) одной локации, и поиск (GET) данной локации.
+    place_id = api_test.test_create_location(expected_status_code=200)
+    api_test.test_find_location(place_id, expected_status_code=200)
 
-    """
-    Считываем значение список place_id из файла list_of_place_id.
-    Выполняем GET-тест для поиска локаций по идентификатору(place_id).
-    """
-    list_of_place_id = FileManager.read_line_by_line(file_path)
-    for place_id in list_of_place_id:
-        api_test.test_find_location(place_id, 200)
+    # Проверяем, что получим 5 place_id, если создаем 5 локаций.
+    place_id_list = api_test.test_create_multiple_locations(count=5)
+    # Записываем полученный place_id_list в файл.
+    FileManager.write_list(file_path, place_id_list)
+    print(f"\nPlace_id list: {place_id_list}")
+    # Считываем из файла ранее записанный place_id_list.
+    recorded_place_id_list = FileManager.read_line_by_line(file_path)
+    print(f"Recorded place_id list: {recorded_place_id_list}")
+    # Проверяем, что список записался корректно, без артифактов.
+    api_test.test_recorded_place_id_list(place_id_list, recorded_place_id_list)
 
     print("\nTEST PASSED")
 
